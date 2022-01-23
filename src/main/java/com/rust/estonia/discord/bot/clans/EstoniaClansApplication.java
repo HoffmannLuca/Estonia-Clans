4e4c5184caf2a412e5ee3f0932a42e816430b673
HoffmannLuca <luca.hoffmann1000@gmail.com>
2022-01-23 18:38:16 +0100

    Initial Commit: Basic Spring Boot Gradle Project

---
+++ src/main/java/com/rust/estonia/discord/bot/clans/EstoniaClansApplication.java
@@ -0,0 +1,13 @@
+package com.rust.estonia.discord.bot.clans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoniaClansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoniaClansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rust/estonia/discord/bot/clans/EstoniaClansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rust.estonia.discord.bot.clans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoniaClansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
