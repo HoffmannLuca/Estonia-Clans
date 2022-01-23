0e976d6ef4e30dd9d302bddbdc5ccf97be74bb9f
HoffmannLuca <luca.hoffmann1000@gmail.com>
2022-01-23 18:54:06 +0100

    create discord api configuration

--- src/main/java/com/rust/estonia/discord/bot/clans/EstoniaClansApplication.java
+++ src/main/java/com/rust/estonia/discord/bot/clans/EstoniaClansApplication.java
@@ -1,13 +1,47 @@
 package com.rust.estonia.discord.bot.clans;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EstoniaClansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstoniaClansApplication.class, args);
+	}
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	@ConfigurationProperties(value = "discord-api")
+	public DiscordApi discordApi(){
+		DiscordApi api = new DiscordApiBuilder()
+
+				.setToken("TOKEN")
+				.setIntents(
+						Intent.GUILDS,
+						Intent.GUILD_MEMBERS,
+						Intent.GUILD_BANS,
+						Intent.GUILD_EMOJIS,
+						Intent.GUILD_VOICE_STATES,
+						Intent.GUILD_MESSAGES,
+						Intent.GUILD_MESSAGE_REACTIONS,
+						Intent.GUILD_INTEGRATIONS,
+						Intent.GUILD_INVITES
+				)
+
+				.login()
+				.join();
+
+		return api;
 	}
 
 }
