40f45caee22f80fec40f5e2dc0ad87a6d2accbac
HoffmannLuca <luca.hoffmann1000@gmail.com>
2022-02-11 00:53:20 +0100

    add category tags to permission util

--- src/main/java/com/rust/estonia/discord/bot/clans/util/PermissionUtil.java
+++ src/main/java/com/rust/estonia/discord/bot/clans/util/PermissionUtil.java
@@ -1,6 +1,7 @@
 package com.rust.estonia.discord.bot.clans.util;
 
 import com.rust.estonia.discord.bot.clans.data.service.SetupService;
+import org.javacord.api.entity.channel.ChannelCategory;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
@@ -24,34 +25,50 @@ public class PermissionUtil {
 
     private Logger logger = LoggerFactory.getLogger(PermissionUtil.class);
 
+    public final String CLAN_CATEGORY = "clan-category";
+    public final String[] categoryTags = {CLAN_CATEGORY};
+
     public final String ADMIN_COMMAND_CHANNEL = "admin-command-channel";
     public final String CLAN_COMMAND_CHANNEL = "clan-command-channel";
+    public final String[] channelTags = {ADMIN_COMMAND_CHANNEL, CLAN_COMMAND_CHANNEL};
 
     public final String ADMIN_ROLE = "admin";
     public final String MODERATOR_ROLE = "moderator";
     public final String CLAN_LEADER_ROLE = "clan-leader";
     public final String CLAN_OFFICER_ROLE = "clan-officer";
+    public final String[] roleTags = {ADMIN_ROLE, MODERATOR_ROLE, CLAN_LEADER_ROLE, CLAN_OFFICER_ROLE};
+
+    private boolean isValidCategoryTag(String categoryTag){
+
+        for (String tag : categoryTags) {
+            if (tag.equalsIgnoreCase(categoryTag)) {
+                return true;
+            }
+        }
+        logger.error(categoryTag+" is not a valid category tag!");
+        return false;
+    }
 
     private boolean isValidChannelTag(String channelTag){
-        if(channelTag.equals(ADMIN_COMMAND_CHANNEL)
-                || channelTag.equals(CLAN_COMMAND_CHANNEL)){
-            return true;
-        } else {
-            logger.error(channelTag+" is not a valid channel tag!");
-            return false;
+
+        for (String tag : channelTags) {
+            if (tag.equalsIgnoreCase(channelTag)) {
+                return true;
+            }
         }
+        logger.error(channelTag+" is not a valid channel tag!");
+        return false;
     }
 
     private boolean isValidRoleTag(String roleTag){
-        if(roleTag.equals(ADMIN_ROLE)
-                || roleTag.equals(MODERATOR_ROLE)
-                || roleTag.equals(CLAN_LEADER_ROLE)
-                || roleTag.equals(CLAN_OFFICER_ROLE)){
-            return true;
-        } else {
-            logger.error(roleTag+" is not a valid role tag");
-            return false;
+
+        for (String tag : roleTags) {
+            if (tag.equalsIgnoreCase(roleTag)) {
+                return true;
+            }
         }
+        logger.error(roleTag+" is not a valid role tag!");
+        return false;
     }
 
     public boolean isCorrectChannel(Server server, TextChannel channel, String channelTag){
@@ -60,6 +77,9 @@ public class PermissionUtil {
     }
     public boolean isCorrectChannel(Server server, TextChannel channel, String channelTag, boolean sendErrorMessage){
 
+        if(!setupService.arePermissionsActive(server)){
+            return true;
+        }
         if(isValidChannelTag(channelTag)){
             TextChannel correctChannel = setupService.getServerTextChannelByChannelTag(server, channelTag);
             if(correctChannel != null){
@@ -82,6 +102,9 @@ public class PermissionUtil {
 
     public boolean userHasRole(Server server, TextChannel channel, User user, String roleTag, boolean sendErrorMessage){
 
+        if(!setupService.arePermissionsActive(server)){
+            return true;
+        }
         if(isValidRoleTag(roleTag)){
             Role role = setupService.getServerRoleByRoleTag(server, roleTag);
             if(role != null){
@@ -95,6 +118,15 @@ public class PermissionUtil {
             }
         }
         return false;
+    }
+
+    public void assignCategoryToServerCategoryTag(Server server, ChannelCategory category, String categoryTag){
+
+        if(isValidCategoryTag(categoryTag)){
+            if(!setupService.setServerCategoryByCategoryTag(server, category, categoryTag)){
+                logger.error("no such category available in this server");
+            }
+        }
     }
 
     public void assignChannelToServerChannelTag(Server server, TextChannel channel, String channelTag){
