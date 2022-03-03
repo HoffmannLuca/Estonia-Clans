d5d00837627a0e653286f57ee9c035579b266af3
HoffmannLuca <luca.hoffmann1000@gmail.com>
2022-03-03 13:09:51 +0100

    create global and server slash command interfaces

---
+++ src/main/java/com/rust/estonia/discord/bot/clans/command/slash/global/GlobalSlashCommand.java
@@ -0,0 +1,33 @@
+package com.rust.estonia.discord.bot.clans.command.slash.global;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+
+import java.util.List;
+
+public interface GlobalSlashCommand {
+
+    String getName();
+
+    SlashCommandBuilder getCommandBuilder();
+
+    void performCommand(SlashCommandInteraction interaction,
+                        SlashCommandInteractionOption firstOption, SlashCommandInteractionOption secondOption, List<SlashCommandInteractionOption> commandArguments,
+                        User user, TextChannel channel);
+
+    default void deactivateCommand(DiscordApi api) {
+
+        api.getGlobalSlashCommands().thenAccept(slashCommands ->
+                slashCommands.stream().filter(slashCommand ->
+                        slashCommand.getName().equals(getName())
+                ).forEach(SlashCommand::deleteGlobal)
+        );
+    }
+}
---
+++ src/main/java/com/rust/estonia/discord/bot/clans/command/slash/server/ServerSlashCommand.java
@@ -0,0 +1,33 @@
+package com.rust.estonia.discord.bot.clans.command.slash.server;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+
+import java.util.List;
+
+public interface ServerSlashCommand {
+
+    String getName();
+
+    SlashCommandBuilder getCommandBuilder();
+
+    void performCommand(SlashCommandInteraction interaction,
+                        SlashCommandInteractionOption firstOption, SlashCommandInteractionOption secondOption, List<SlashCommandInteractionOption> commandArguments,
+                        User user, TextChannel channel, Server server);
+
+    default void deactivateCommand(Server server) {
+
+        server.getSlashCommands().thenAccept(slashCommands ->
+                slashCommands.stream().filter(slashCommand ->
+                        slashCommand.getName().equals(getName())
+                ).forEach(slashCommand ->
+                        slashCommand.deleteForServer(server)
+                )
+        );
+    }
+
+}
