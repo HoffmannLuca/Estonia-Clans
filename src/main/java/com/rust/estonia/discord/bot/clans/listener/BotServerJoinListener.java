cefd8033f8d747034d70eba74e18801f0920b5b9
HoffmannLuca <luca.hoffmann1000@gmail.com>
2022-03-03 14:25:54 +0100

    create bot server join listener to enable server commands

---
+++ src/main/java/com/rust/estonia/discord/bot/clans/listener/BotServerJoinListener.java
@@ -0,0 +1,32 @@
+package com.rust.estonia.discord.bot.clans.listener;
+
+import com.rust.estonia.discord.bot.clans.command.slash.server.ServerSlashCommand;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.server.ServerJoinEvent;
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.listener.server.ServerJoinListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BotServerJoinListener implements ServerJoinListener {
+
+    @Autowired
+    private List<ServerSlashCommand> serverSlashCommands;
+
+    @Override
+    public void onServerJoin(ServerJoinEvent event) {
+
+        List<SlashCommandBuilder> serverCommandBuilder = new ArrayList<>();
+        for(ServerSlashCommand command : serverSlashCommands){
+            serverCommandBuilder.add(command.getCommandBuilder());
+        }
+        for(Server server : event.getApi().getServers()){
+            event.getApi().bulkOverwriteServerApplicationCommands(server, serverCommandBuilder);
+        }
+
+    }
+}
